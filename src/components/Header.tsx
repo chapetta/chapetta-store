f60f4c010127fba7a0b79c7c679b3aa4e00312c7
chapetta <cha.petta@hotmail.com>
2025-09-30 19:45:36 -0300

    implementando o componente Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import logo from '../assets/logo/hashtag.svg'
+
+export const Header = () => {
+  return (
+    <header>
+      <a href="/home"><img src={logo} alt="imagem de logo" /></a>
+    </header>
+  );
+};
