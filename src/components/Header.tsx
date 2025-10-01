cceb976202fb4f5415f79e0cce005cd2682291bb
chapetta <cha.petta@hotmail.com>
2025-10-01 07:38:21 -0300

    fazendo pequenas alteraçoes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { UserButtons } from './userButtons';
 
 export const Header = () => {
   return (
-    <header className='flex text-xl sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200'>
+    <header className='flex sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-9 py-4 items-end justify-between lg:text-base '>
       <a href="/home"><img className='h-16 px-2' src={logo} alt="imagem de logo"  /></a>
       <UserButtons />
     </header>
--- src/components/userButtons.tsx
+++ src/components/userButtons.tsx
@@ -1,8 +1,15 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBagShopping, faUser } from '@fortawesome/free-solid-svg-icons'
+
 export const UserButtons = () => {
-    return (
-        <div>
-          <button></button>
-          <a></a>
-        </div>
-    );
+  return (
+    <div>
+      <button className='px-2'>
+        <FontAwesomeIcon icon={faBagShopping} />
+      </button>
+      <a className='px-2'>
+        <FontAwesomeIcon icon={faUser} />
+      </a>
+    </div>
+  );
 };
