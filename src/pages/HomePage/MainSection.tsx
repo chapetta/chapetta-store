e6cd06561db7627acd595537b259c6709d1ffebd
chapetta <cha.petta@hotmail.com>
2025-10-01 09:43:33 -0300

    implementando o product filters e adicionando a pagina para filtrar os cards

--- src/pages/HomePage/MainSection.tsx
+++ src/pages/HomePage/MainSection.tsx
@@ -1,9 +1,10 @@
+import { ProductFilters } from "./PRoductFilters";
 import { ProductsContainer } from "./ProductsContainer";
 
 export const MainSection = () => {
   return (
     <>
-      <h1>Filtros</h1>
+      <ProductFilters />
       <ProductsContainer />
     </>
   );
