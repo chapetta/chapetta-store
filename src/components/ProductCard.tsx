86a1f97a7ac62122160815d8446cbc88309e4314
chapetta <cha.petta@hotmail.com>
2025-10-01 08:57:34 -0300

    estilizando o prodcut card com tailwindcss

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -7,13 +7,18 @@ type ProductCard = {
   price: number
 }
 
-export const ProductCard = ({id, name, brand, image, feminine, price }: ProductCard) => {
-    return (
-        <article>
-          <img src={image} alt={`imagem do produto ${id}`} />
-          <p>{brand}</p>
-          <p>{name}</p>
-          <p>{price}</p>
-        </article>
-    );
+export const ProductCard = ({ id, name, brand, image, feminine, price }: ProductCard) => {
+  return (
+    <article className="card-produto group w-48 bg-stone-100 shadow-xl shadow-slate-200 flex flex-col justify-around border-2 border-gray-200 rounded-lg ">
+      <img
+        src={image}
+        alt={`imagem do produto ${id}`}
+        className=" group-hover:scale-110 mx-6 my-4 transition-transform duration-300 transform"
+      />
+      <p className="mx-4 text-sm text-slate-400">{brand}</p>
+      <p className="mx-4 text-sm ">{name}</p>
+      <p className="mx-4 text-sm  text-green-700 ">${price}</p>
+    </article>
+
+  );
 };
