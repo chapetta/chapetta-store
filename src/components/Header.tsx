149424f34316de2a7908804e841f825dad6ab8ba
chapetta <cha.petta@hotmail.com>
2025-10-06 07:18:33 -0300

    criaçao do header simples e complete header para aparecer em diferentes pagians dependendo da condição

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,26 @@
+import { useLocation } from 'react-router-dom';
 import logo from '../assets/logo/hashtag.svg'
 import { UserButtons } from './userButtons';
 
 export const Header = () => {
-  return (
+  const { pathname } = useLocation();
+  const completeHeader = (
     <header className='flex sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-9 py-4 items-end justify-between text-xl z-10 '>
       <a href="/home"><img className='h-16 px-2' src={logo} alt="imagem de logo"  /></a>
       <UserButtons />
     </header>
   );
+  
+  const simpleHeader = (
+    <header className='flex sticky top-0 shadow-xl shadow-slate-400 bg-slate-950 text-slate-200 px-9 py-4 items-end justify-between text-xl z-10 '>
+      <a href="/home"><img className='h-12 px-2' src={logo} alt="imagem de logo"  /></a>
+    </header>
+ )
+
+ return (
+  <>
+   {pathname === '/' ? completeHeader : simpleHeader}
+  </>
+ )
+ 
 };
