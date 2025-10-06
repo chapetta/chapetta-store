1a19879fd06aec4f93c78860cfcd7475fdec7946
chapetta <cha.petta@hotmail.com>
2025-10-06 09:43:14 -0300

    aplicando a funcionalidade de abrir o carrinho ao clicar no icone do carrinho

--- src/components/userButtons.tsx
+++ src/components/userButtons.tsx
@@ -1,11 +1,14 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBagShopping, faUser } from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom';
+import { useCart } from '../UseHooks/UseCart';
 
 export const UserButtons = () => {
+  const {setIsCartOpen } = useCart();
+
   return (
     <div>
-      <button className='px-2 relative'>
+      <button className='px-2 relative' onClick={() => setIsCartOpen(true)}>
         <FontAwesomeIcon icon={faBagShopping} />
         <div className='absolute inline-flex justify-center items-center w-6 h-6 text-white font-bold bg-red-500 border-2 border-white rounded-full -top-2 -right-2'>3</div>
       </button>
