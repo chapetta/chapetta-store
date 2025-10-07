cc4688359e9316b14f05436df776e93868e1e505
chapetta <cha.petta@hotmail.com>
2025-10-07 13:28:51 -0300

    implementação do button serch

--- src/components/InputSearch/index.tsx
+++ src/components/InputSearch/index.tsx
@@ -1,12 +1,19 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+
 export const InputSearch = () => {
   return (
-    <label htmlFor="search" className="flex  justify-center m-20">
-      <input
-        type="text"
-        placeholder="Pesquiste um produto"
-        id="search"
-        name="search"
-        className="text-center border-2" />
-    </label>
+    <div className="flex m-0 justify-center m-20">
+
+      <label htmlFor="search" className="flex  justify-center">
+        <input
+          type="text"
+          placeholder="Pesquiste um produto"
+          id="search"
+          name="search"
+          className="text-center border-2" />
+      </label>
+     <button className="cursor-pointer"> <FontAwesomeIcon icon={faSearch}/></button>
+    </div>
   );
 };
