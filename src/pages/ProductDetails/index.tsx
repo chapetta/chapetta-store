ec5ec7b4af2efa6ca8d2d4c748d7c8ecd11b9980
chapetta <cha.petta@hotmail.com>
2025-10-08 18:00:04 -0300

    filtrando o produto pelo id do params

--- src/pages/ProductDetails/index.tsx
+++ src/pages/ProductDetails/index.tsx
@@ -1,8 +1,13 @@
+import { useContext } from "react";
 import { useParams } from "react-router-dom";
+import { ProductContext } from "../../contexts/ProductContext";
 
 export const ProductDetails = () => {
 
     const { id } = useParams();
+    const { products } = useContext(ProductContext)
+    const productDetail = products.find((product) => product.id === Number(id))
+    console.log(productDetail)
     console.log(id)
     return (
         <div>ProductDetails</div>
