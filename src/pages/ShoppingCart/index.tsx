2362626f7017f9ae7a0b4924a2ced3d91f23e851
chapetta <cha.petta@hotmail.com>
2025-10-10 11:55:12 -0300

    refatorando a pagina de shopping cart

--- src/pages/ShoppingCart/index.tsx
+++ src/pages/ShoppingCart/index.tsx
@@ -1,27 +1,38 @@
-
+import { Box, Typography } from "@mui/material";
 import { useCart } from "../../UseHooks/UseCart";
 import { CartItemCard } from "../../components/Cart/CartItemCard";
 
-
-
 export const ShoppingCart = () => {
+  const { cartList } = useCart();
 
-    const { cartList } = useCart()
-
-    return (
-        <section className="flex flex-col justify-center items-center w-full"
-      >
-        {cartList.length === 0 ? (
-          <h3>Seu carrinho esta vazio</h3>
-        ) : (
-          cartList.map((product) => (
-            <CartItemCard 
-              key={`product key ${product.id}`}
-              {...product}
-            />
-          ))
-        )}
-
-      </section>
-    );
+  return (
+    <Box
+      sx={{
+        height: 400,               
+        overflowY: "auto",       
+        overflowX: "hidden",      
+        pr: 1,                    
+        "&::-webkit-scrollbar": { 
+          width: "6px",
+        },
+        "&::-webkit-scrollbar-thumb": {
+          backgroundColor: "#888",
+          borderRadius: "8px",
+        },
+      }}
+    >
+      {cartList.length === 0 ? (
+        <Typography variant="h6" align="center" sx={{ mt: 2 }}>
+          Seu carrinho está vazio 🛒
+        </Typography>
+      ) : (
+        cartList.map((product) => (
+          <CartItemCard
+            key={`product-${product.id}`}
+            {...product}
+          />
+        ))
+      )}
+    </Box>
+  );
 };
