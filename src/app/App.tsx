cfa66019f9533c4b277f61b0c3e180017a083d37
chapetta <cha.petta@hotmail.com>
2025-10-13 17:34:40 -0300

    refatorando o app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,18 +4,6 @@ import { ProductsProvider } from '@/providers/ProductsProvider'
 import { Router } from '@/routes'
 
 function App() {
-  // const handleButtonSearch = async (query: string) => {
-  //   setLoading(true)
-
-  //   const getProductsFromInputSearch = await getProductsFromQuery(query)
-  //   const delay = new Promise((resolve) => setTimeout(resolve, 1000))
-
-  //   const [results] = await Promise.all([getProductsFromInputSearch, delay])
-
-  //   setProducts(results)
-  //   setLoading(false)
-  // }
-
   return (
     <>
       <ProductsProvider>
